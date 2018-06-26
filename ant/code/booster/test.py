bc43360b9822f4133a81ba339a5db674527859d1
Leo <zhihao.cui@student.uts.edu.au>
2018-06-27 00:28:00 +1000

    vers3

--- ant/code/booster/test.py
+++ ant/code/booster/test.py
@@ -23,6 +23,10 @@ train_path = "../../data/train.csv" #train_heatmap , train_mode_fill, train,
 #test_data path
 test_path = "../../data/test_a.csv" #test_a_heatmap, test_a_mode_fill, test_a,
 
+params_path = "model_params/{}_{}_{}:{}/".format(now.month, now.day, now.hour, now.minute)
+score_path = "score/{}_{}_{}:{}/".format(now.month, now.day, now.hour, now.minute)
+as_path = "../../tool/answer_sheet.csv"
+
 #Pd read path
 train_data = pd.read_csv(train_path)
 test = pd.read_csv(test_path)
@@ -37,12 +41,34 @@ def custom_imputation(df_train, df_test, fillna_value = None):
     test = df_test.fillna(fillna_value)
     return train, test
 
+def save_score(preds):
+    low_risk = 0
+    medium_risk = 0
+    high_risk = 0
+    for p in preds:
+        if p > 0.2 and p < 0.4:
+            low_risk += 1
 
+        elif p >= 0.4 and p < 0.6:
+            medium_risk += 1
 
+        elif p >= 0.6:
+            high_risk += 1
+
+    print("probability [0.2 ~ 0.4] rate : {:.3f}%\n".format(100*(low_risk/len(preds))),
+          "probability [0.4 ~ 0.6] rate : {:.3f}%\n".format(100*(medium_risk/len(preds))),
+          "probability [0.6 ~ 1.0] rate : {:.3f}%\n".format(100*(high_risk/len(preds))))
+
+    answer_sheet = pd.read_csv(as_path)
+    answer_sheet = pd.DataFrame(answer_sheet)
+    answer = answer_sheet.assign(score = preds)
+    answer.to_csv(score_path + "{}_score_{}d{}m{}h.csv".format(param_name, now.day, now.month, now.hour), index = None, float_format = "%.9f")
+    return print("Score saved in {}".format(score_path))
 
 def main():
-
-    #train, test = custom_imputation(train, test, fillna_value = 0)
+    start = time.time()
+    os.makedirs(score_path)
+    train, test = custom_imputation(train, test, fillna_value = 1)
     #split data
     #train, validation, label, validation_label = train_test_split(train, labels, test_size = 0.3, random_state = 42)
     #feature preprocessing
@@ -50,17 +76,25 @@ def main():
     standar = StandardScaler(with_mean=True, with_std=True)
     norm = Normalizer(norm = 'l2') #norm l1, l2
     #feature selection
-    tbfs = XGBClassifier() #tree base feature_selection
+    tbfs = XGBClassifier(max_depth = 3, n_estimators = 400, subsample = 0.8,
+                        colsample_bytree = 0.8, learning_rate = 0.08, n_jobs = -1) #tree base feature_selection
     kbest = SelectKBest(chi2)
     #clf
-    xgb = XGBClassifier(n_jobs = -1)
-    rdforest = RandomForestClassifier()
-    grdboost = GradientBoostingClassifier()
+    xgb = XGBClassifier(max_depth = 3, n_estimators = 400, subsample = 0.9,
+                        colsample_bytree = 0.8, learning_rate = 0.1, n_jobs = -1)
+    rdforest = RandomForestClassifier(n_jobs = -1)
+    grdboost = GradientBoostingClassifier(n_jobs = -1)
 
-    params = [{"tbfs__n_estimators" : [10, 12]},
-              {"xgb__n_estimators" : [14, 14]}]
+    # ###########################Tuning Params################################
 
-    pipe = Pipeline(steps = [('imputer', Imputer()),
+    params = [
+              [{"tbfs__min_child_weight" : [1,2,3]}, {"xgb__max_depth" : [3, 4], "xgb__min_child_weight" : [1, 2, 3]}],
+              [{"tbfs__subsample" : [0.9, 0.8]}, {"xgb__gamma" : [0.1, 0.2], "xgb__subsample" : [0.9, 0.8, 0.7], "xgb__colsample_bytree" : [0.9, 0.8, 0.7]}],
+              [{"tbfs__colsample_bytree" : [0.8, 0.7]}, {"xgb__reg_alpha" : [0.01, 0.05, 0.06], "xgb__scale_pos_weight" : [1, 10, 20]}],
+              [{"xgb__learning_rate" : [i*0.01 for i in range(3,9)]}]
+             ]
+
+    pipe = Pipeline(steps = [ #('imputer', Imputer()),
                                ('tbfs', SelectFromModel(tbfs)),
     	                       ('xgb', xgb)])
 
@@ -73,19 +107,34 @@ def main():
     	                       #('xgb', xgb)])
 
     for param in params:
-    	clf = GridSearchCV(pipe, param_grid  = param,  scoring = 'roc_auc',
+    	clf = GridSearchCV(pipe, param_grid  = param, scoring = 'roc_auc',
                            verbose = 1, n_jobs = -1, cv = 5)
 
-    print("\nBest parameters set found on development set:")
+    clf = clf.fit(train, labels)
     bst_params = clf.best_params_
     bst_score = clf.best_score_
     bst_estimator = clf.best_estimator_
-    print(bst_score)
-
-    clf = bst_estimator.fit(train, labels)
+    print("\nBest parameters set found on development set: + \n +{}".format(bst_params))
     probs = clf.predict_proba(test)
-    print(probs)
+    #save score
+    save_score(probs[1])
+
+    # ###############################save params################################
+
+    with open(params_path  + "params.txt", 'a') as f:
+        f.write(
+                "************************" + "\n"
+                + str(bst_estimator) + "\n"
+                + str(bst_params) + "\n"
+                + str(bst_score))
+
+    print("Find best params {}, with best roc {}".format(bst_params, bst_score))
+    plot.grid_search(clf.grid_scores_, change='max_depth', kind ='bar')
+    plt.savefig(params_path + "grid_params_{}.png".format(round(bst_score,2)))
+    end = time.time()
+    print(">>>>Duration<<<< : {}min ".format(round((end-start)/60,2)))
 
 if __name__ == '__main__':
-    warnings.filterwarnings(module = 'sklearn*', action = 'ignore', category = DeprecationWarning)
+    warnings.filterwarnings(module = 'sklearn*',
+                            action = 'ignore', category = DeprecationWarning)
     main()
