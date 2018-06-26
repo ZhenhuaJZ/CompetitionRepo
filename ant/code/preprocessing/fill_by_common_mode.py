d43266a193e1cf63b3fbdf942edffa0b8d955c32
Leo Cui <lecui@orion7.eng.uts.edu.au>
2018-06-26 14:50:48 +1000

    changed col names

--- ant/code/preprocessing/fill_by_common_mode.py
+++ ant/code/preprocessing/fill_by_common_mode.py
@@ -38,7 +38,7 @@ def find_common_mode(black_frequency_list, white_frequency_list):
                 break;
             # Calculate the difference in frequency, the number with smallest frequency is stored in min_mode_value
             freq_diff = abs((white_frequency_list.iloc[i] - black_frequency_list.iloc[j])*100)
-            # print(freq_diff)
+            #print(freq_diff)
             if (freq_diff < min_freq_diff) and (white_frequency_list.index[i] == black_frequency_list.index[j]):
                 min_mode_value = white_frequency_list.index[i]
     common_mode = min_mode_value
@@ -51,37 +51,40 @@ def find_common_mode(black_frequency_list, white_frequency_list):
 
 def replace_missing_by_custom_mode(black_data,white_data,test_data):
     print("Initiate custom mode filling nan process")
-    for i in range(1, 297):
-        if black_data["f{}".format(i)].mode()[0] == white_data["f{}".format(i)].mode()[0]:
-            common_mode = black_data["f{}".format(i)].mode()[0]
+    for i in range(black_data.shape[1]-3):
+        col_name = black_data.columns.values.tolist()[3:]
+        if black_data[col_name[i]].mode()[0] == white_data[col_name[i]].mode()[0]:
+        #if black_data["f{}".format(i)].mode()[0] == white_data["f{}".format(i)].mode()[0]:
+            #common_mode = black_data["f{}".format(i)].mode()[0]
+            common_mode = black_data[col_name[i]].mode()[0]
         else:
             # Calculate a list of occurrence in each feature
-            black_mode_list = black_data["f{}".format(i)].value_counts() / len(black_data["f{}".format(i)])
-            white_mode_list = white_data["f{}".format(i)].value_counts() / len(white_data["f{}".format(i)])
+            black_mode_list = black_data[col_name[i]].value_counts() / len(black_data[col_name[i]])
+            white_mode_list = white_data[col_name[i]].value_counts() / len(white_data[col_name[i]])
             # Calculate the common occurrence between black and white data
             common_mode = find_common_mode(black_mode_list,white_mode_list)
-        black_data["f{}".format(i)] = black_data["f{}".format(i)].fillna(black_data["f{}".format(i)].mode()[0])
-        white_data["f{}".format(i)] = white_data["f{}".format(i)].fillna(white_data["f{}".format(i)].mode()[0])
-        test_data["f{}".format(i)] = test_data["f{}".format(i)].fillna(common_mode)
-        print("Filled feature {}".format(i) + "***Black Filled:{}".format(black_data["f{}".format(i)].mode()[0]) + "***White Filled:{}".format(white_data["f{}".format(i)].mode()[0]) + "***Test Filled:{}".format(common_mode))
+        black_data[col_name[i]] = black_data[col_name[i]].fillna(black_data[col_name[i]].mode()[0])
+        white_data[col_name[i]] = white_data[col_name[i]].fillna(white_data[col_name[i]].mode()[0])
+        test_data[col_name[i]] = test_data[col_name[i]].fillna(common_mode)
+        print("Filled feature {}".format(col_name[i]) + "***Black Filled:{}".format(black_data[col_name[i]].mode()[0]) + 
+              "***White Filled:{}".format(white_data[col_name[i]].mode()[0]) + "***Test Filled:{}".format(common_mode))
         print("******************************")
     print("End of custom mode filling")
     return black_data, white_data, test_data
 
 def main():
-    black_data = pd.read_csv("../../data/black_label_w_missing.csv")
+    black_data = pd.read_csv("data/train_heatmap_1.csv")
     print("loaded black data")
-    white_data = pd.read_csv("../../data/white_label_w_missing.csv")
+    white_data = pd.read_csv("data/train_heatmap_0.csv")
     print("loaded white data")
-    test_data = pd.read_csv("../../data/test_a.csv")
+    test_data = pd.read_csv("data/test_a_heatmap.csv")
     print("loaded test data")
     black_data_filled, white_data_filled, test_data_filled = replace_missing_by_custom_mode(black_data,white_data,test_data)
-    exit()
-    black_data_filled.to_csv("black_data_w_mode_filled.csv")
+    black_data_filled.to_csv("data/train_1_mode_fill.csv", index = None)
     print("saved black data")
-    white_data_filled.to_csv("white_data_w_mode_filled.csv")
+    white_data_filled.to_csv("data/train_0_mode_fill.csv", index = None)
     print("saved white data")
-    test_data_filled.to_csv("test_data_w_mode_filled.csv")
+    test_data_filled.to_csv("data/test_a_mode_fill.csv", index = None)
     print("saved test data")
     print("end of program")
 
