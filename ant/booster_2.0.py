aed29280ad9ba9fd0b4b3898a5dd237881a377f5
Leo <zhihao.cui@student.uts.edu.au>
2018-06-29 02:56:32 +1000

    changed #

--- ant/booster_2.0.py
+++ ant/booster_2.0.py
@@ -23,7 +23,7 @@ _train_data, _test_online = custom_imputation(_train_data, _test_online, fillna_
 #change -1 label to 1
 _train_data.loc[_train_data["label"] == -1] = 1
 #Split train and offine test
-_train_data, _test_offline =  test_train_split_by_date(_train_data, 20171001, 20171010, params_path)
+_train_data, _test_offline =  test_train_split_by_date(_train_data, 20171010, 20171020, params_path)
 #train data
 _train = _train_data.iloc[:,3:]
 _labels = _train_data.iloc[:,1]
