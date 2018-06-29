123667c7bef33a8441e032ef77435788278b2f30
UTSAccount <zhenhua.zhang@student.uts.edu.au>
2018-06-29 17:51:35 +1000

    booster

--- ant/booster_2.0.py
+++ ant/booster_2.0.py
@@ -6,7 +6,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.externals import joblib
 from lib.data_processing import save_score, test_train_split_by_date, creat_project_dirs, custom_imputation
 from lib.model_performance import offline_model_performance
-import datetime, time
+import datetime
 now = datetime.datetime.now()
 
 log_path = "log/date_{}/{}:{}_SM/".format(now.day,now.hour,now.minute)
@@ -44,61 +44,42 @@ _test_offline_labels = _test_offline.iloc[:,1]
 
 del _train_data, _test_offline
 
-def retrain_all_dataset(train, test):
-	clf.fit(_train, _labels)
-	probs = clf.predict_proba(_test_online)
-	joblib.dump(clf, model_path + "{}.pkl".format("model"))
-	save_score(probs[:,1], score_path)
+classifier = {
+	"XGB" : XGBClassifier(max_depth = 4, n_estimators = 480, subsample = 0.8, gamma = 0.1,
+						 scale_pos_weight =20, min_child_weight = 2,
+						 colsample_bytree = 0.8, learning_rate = 0.08, n_jobs = -1),
+
+  	"logistic_regression" : LogisticRegression(penalty = "l2", C = 1, solver = "newton-cg",
+  						 class_weight = "balanced", max_iter = 300, n_jobs = -1),
+
+	"random_forest" : RandomForestClassifier(n_estimators = 60, criterion = "entropy", max_depth = 13,
+	 					 min_samples_split = 110, min_samples_leaf = 1, max_leaf_nodes = None),
+
+	"MLP" : MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
+					     beta_1=0.9, beta_2=0.999, early_stopping=False,
+					     epsilon=1e-08, hidden_layer_sizes=(5, 2), learning_rate='constant',
+					     learning_rate_init=0.001, max_iter=200, momentum=0.9,
+					     nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
+					     solver='lbfgs', tol=0.0001, validation_fraction=0.1)
+}
+
+clf = classifier["logistic_regression"]
+
+# with open(params_path  + "params.txt", 'a') as f:
+# 	f.write(
+# 			"**"*40 + "\n"*2
+# 			+ str(clf) + "\n"*2
+# 			+"**"*40 + "\n"*2
+# 			)
+
+clf = clf.fit(_train, _labels)
+probs = clf.predict_proba(_test_online)
+# joblib.dump(clf, model_path + "{}.pkl".format("model"))
+offline_score = offline_model_performance(clf, _test_offline_feature, _test_offline_labels, params_path)
+# save_score(probs[:,1], score_path)
 
 def main():
-	start = time.time()
-	classifier = {
-		"XGB" : XGBClassifier(max_depth = 4, n_estimators = 480, subsample = 0.8, gamma = 0.1,
-							 scale_pos_weight =20, min_child_weight = 2,
-							 colsample_bytree = 0.8, learning_rate = 0.08, n_jobs = -1),
 
-	  	"logistic_regression" : LogisticRegression(penalty = "l2", C = 1, solver = "newton-cg",
-	  						 class_weight = "balanced", max_iter = 300, n_jobs = -1),
+	if __name__ == '__main__':
 
-		"random_forest" : RandomForestClassifier(n_estimators = 300, criterion = "entropy", max_depth = 13,
-		 					 min_samples_split = 110, min_samples_leaf = 20, max_leaf_nodes = None),
-
-		"MLP" : MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
-						     beta_1=0.9, beta_2=0.999, early_stopping=False,
-						     epsilon=1e-08, hidden_layer_sizes=(5, 2), learning_rate='constant',
-						     learning_rate_init=0.001, max_iter=200, momentum=0.9,
-						     nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
-						     solver='lbfgs', tol=0.0001, validation_fraction=0.1)
-	}
-
-	clf = classifier["random_forest"]
-
-	with open(params_path  + "params.txt", 'a') as f:
-		print("\n# Training clf :{}".format(clf))
-		f.write(
-		"**"*40 + "\n"*2
-		+ str(clf) + "\n"*2
-		+"**"*40 + "\n"*2
-		)
-	#train without validation_set
-
-	clf = clf.fit(_train, _labels)
-	probs = clf.predict_proba(_test_online)
-	joblib.dump(clf, model_path + "{}.pkl".format("model"))
-	offline_score = offline_model_performance(clf, _test_offline_feature, _test_offline_labels, params_path)
-	save_score(probs[:,1], score_path)
-	"""
-	# TODO: add xx in the future
-	_train_data = pd.read_csv(train_path)
-	_test_online = pd.read_csv(test_path)
-
-	_train_data, _test_online = custom_imputation(_train_data, _test_online, fillna_value)
-	#change -1 label to 1
-	_train_data.loc[_train_data["label"] == -1] = 1
-
-	"""
-
-	print("\n# >>>>Duration<<<< : {}min ".format(round((time.time()-start)/60,2)))
-
-if __name__ == '__main__':
-	main()
+		main()
