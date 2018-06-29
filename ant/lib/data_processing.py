fe110ed07865650b0e7dbe70c4fa6fd9d0294694
Leo <zhihao.cui@student.uts.edu.au>
2018-06-29 20:47:39 +1000

    v_1.0.1

--- ant/lib/data_processing.py
+++ ant/lib/data_processing.py
@@ -60,12 +60,14 @@ def test_train_split_by_date(data, start_y_m_d, end_y_m_d, params_path = ""):
     return data, split_data
 
 # This function merges two dataframe and can be sort by provided string
-def file_merge(data_1, data_2, sort_by = 0, reset_index = False):
+def file_merge(data_1, data_2, sort_by = "", reset_index = False):
     merged_file = pd.concat([data_1,data_2], axis = 0)
-    if sort_by != 0:
-        merged_file = merged_file.sort_values(by = sort_by)
+    if sort_by != "":
+        merged_file = merged_file.sort_values(by = str(sort_by))
+        print("# Merged data {} and {} in {} order".format(data_1, data_2, sort_by))
     if reset_index:
         merged_file.reset_index()
+        print("# Merged data and sort in <Index Order>)
     del data_1, data_2
     return merged_file
 
