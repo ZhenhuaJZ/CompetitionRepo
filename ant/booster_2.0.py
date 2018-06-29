15261fe395046d1ff2ffbea5bcd405da13309b7b
Leo <zhihao.cui@student.uts.edu.au>
2018-06-30 09:56:25 +1000

    v_1.0.1

--- ant/booster_2.0.py
+++ ant/booster_2.0.py
@@ -121,7 +121,7 @@ classifier = {
   						 class_weight = "balanced", max_iter = 300, n_jobs = -1),
 
 	# NOTE:test min_samples_split and min_samples_leaf
-	"random_forest" : RandomForestClassifier(n_estimators = 150, criterion = "entropy", max_depth = 13,
+	"random_forest" : RandomForestClassifier(n_estimators = 300, criterion = "entropy", max_depth = 16,
 	 					 min_samples_split = 110, min_samples_leaf = 1, max_leaf_nodes = None,
 						 n_jobs = -1),
 
