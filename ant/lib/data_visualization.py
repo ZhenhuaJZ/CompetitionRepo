1c49ad6825804927e08dd88a37bea5c73402dfd8
Leo <zhihao.cui@student.uts.edu.au>
2018-06-29 16:34:15 +1000

    v_1.0.1

--- ant/lib/data_visualization.py
+++ ant/lib/data_visualization.py
@@ -76,13 +76,14 @@ def main():
 
 	_train_data = pd.read_csv(train_path)
 	_test_online = pd.read_csv(test_path)
+
+	_train_data, _test_offline =  test_train_split_by_date(_train_data, 20171010, 20171020)
+
 	_train = _train_data.iloc[:,3:]
 	_test_online = _test_online.iloc[:,2:]
 
-	_train, _test_offline =  test_train_split_by_date(_train, 20171010, 20171020)
-
-	hist_visualization(_train, _test_online, "train_test_b", figure_1="train", figure_2="test_b")
-	hist_visualization(_test_offline, _test_online, "test_offlien_test_b", figure_1="offline", figure_2="online")
+	#hist_visualization(_train, _test_online, "train_test_b", figure_1="train", figure_2="test_b")
+	hist_visualization(_test_offline, _test_online, "on-off-1010-1020", figure_1="offline", figure_2="online")
 
 if __name__ == '__main__':
 	main()
