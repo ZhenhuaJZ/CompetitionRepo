cea46e06c6306797c5df14371f9b87edd5363238
Leo <zhihao.cui@student.uts.edu.au>
2018-06-30 19:56:31 +1000

    change performace started with 1

--- ant/lib/model_performance.py
+++ ant/lib/model_performance.py
@@ -10,6 +10,8 @@ def offline_model_performance(estimator, validation_feature, validation_label, p
     #params_path = log_path + "params/"
     # Obtain array of false positive rate and true positive rate
     fpr, tpr, thresholds = roc_curve(validation_label, estimator.predict_proba(validation_feature)[:,1])
+    fpr = fpr[1::2]
+    tpr = tpr[1::2]
     #print("fpr list", fpr[:20])
     #print("tpr list", tpr[:20])
     #print("fpr list lens{}".format(len(fpr)))
@@ -18,6 +20,77 @@ def offline_model_performance(estimator, validation_feature, validation_label, p
     fpr1 = 99
     fpr2 = 99
     fpr3 = 99
+
+    for i in range(0,len(fpr),1):
+        min_dist_val = 0
+        if fpr[i] >= 0.001 and fpr[i] <= fpr1:
+            fpr_1 = fpr[i]
+            tpr_1 = tpr[i]
+            diff_n_1 = abs(fpr[i] - fpr[i-1])
+            diff_p_1 = abs(fpr[i] - fpr[i+1])
+            if (diff_p_1 < diff_n_1) and (diff_p_1 != 0):
+                fpr_2 = fpr[i+1]
+                tpr_2 = tpr[i+1]
+            elif (diff_n_1 < diff_p_1) and (diff_n_1 != 0):
+                fpr_2 = fpr[i-1]
+                tpr_2 = tpr[i-1]
+            elif diff_p_1:
+                fpr_2 = fpr[i+1]
+                tpr_2 = tpr[i+1]
+            elif diff_n_1:
+                fpr_2 = fpr[i-1]
+                tpr_2 = tpr[i-1]
+            else:
+                continue
+            fpr1 = fpr_1
+            print("tpr at 0.001 selected tpr_1:{:7f}, fpr_1:{:7f}, fpr_2:{:7f}, tpr_2:{:7f}".format(tpr_1, fpr_1, fpr_2, tpr_2))
+            tpr1 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(float(0.001)-fpr_1))+tpr_1
+        elif fpr[i] >= 0.005 and fpr[i] <= fpr2:
+            fpr_1 = fpr[i]
+            tpr_1 = tpr[i]
+            diff_n_1 = abs(fpr[i] - fpr[i-1])
+            diff_p_1 = abs(fpr[i] - fpr[i+1])
+            if (diff_p_1 < diff_n_1) and (diff_p_1 != 0):
+                fpr_2 = fpr[i+1]
+                tpr_2 = tpr[i+1]
+            elif (diff_n_1 < diff_p_1) and (diff_n_1 != 0):
+                fpr_2 = fpr[i-1]
+                tpr_2 = tpr[i-1]
+            elif diff_p_1:
+                fpr_2 = fpr[i+1]
+                tpr_2 = tpr[i+1]
+            elif diff_n_1:
+                fpr_2 = fpr[i-1]
+                tpr_2 = tpr[i-1]
+            else:
+                continue
+            fpr2 = fpr_1
+            print("tpr at 0.005 selected tpr_1:{:7f}, fpr_1:{:7f}, fpr_2:{:7f}, tpr_2:{:7f}".format(tpr_1, fpr_1, fpr_2, tpr_2))
+            tpr2 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(float(0.005)-fpr_1))+tpr_1
+        elif fpr[i] >= 0.01 and fpr[i] <= fpr3:
+            fpr_1 = fpr[i]
+            tpr_1 = tpr[i]
+            diff_n_1 = abs(fpr[i] - fpr[i-1])
+            diff_p_1 = abs(fpr[i] - fpr[i+1])
+            if (diff_p_1 < diff_n_1) and (diff_p_1 != 0):
+                fpr_2 = fpr[i+1]
+                tpr_2 = tpr[i+1]
+            elif (diff_n_1 < diff_p_1) and (diff_n_1 != 0):
+                fpr_2 = fpr[i-1]
+                tpr_2 = tpr[i-1]
+            elif diff_p_1:
+                fpr_2 = fpr[i+1]
+                tpr_2 = tpr[i+1]
+            elif diff_n_1:
+                fpr_2 = fpr[i-1]
+                tpr_2 = tpr[i-1]
+            else:
+                continue
+            fpr3 = fpr_1
+            print("tpr at 0.01 selected tpr_1:{:7f}, fpr_1:{:7f}, fpr_2:{:7f}, tpr_2:{:7f}".format(tpr_1, fpr_1, fpr_2, tpr_2))
+            tpr3 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(float(0.01)-fpr_1))+tpr_1
+
+    """
     for i in range(1,len(fpr),2):
         min_dist_val = 0
         if fpr[i] >= 0.001 and fpr[i] <= fpr1:
@@ -40,7 +113,7 @@ def offline_model_performance(estimator, validation_feature, validation_label, p
             else:
                 continue
             fpr1 = fpr_1
-            print("tpr at 0.001 selected tpr_1:{}, fpr_1:{}, fpr_2:{}, tpr_2:{}".format(tpr_1, fpr_1, fpr_2, tpr_2))
+            print("tpr at 0.001 selected tpr_1:{:7f}, fpr_1:{:7f}, fpr_2:{:7f}, tpr_2:{:7f}".format(tpr_1, fpr_1, fpr_2, tpr_2))
             tpr1 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(float(0.001)-fpr_1))+tpr_1
         elif fpr[i] >= 0.005 and fpr[i] <= fpr2:
             fpr_1 = fpr[i]
@@ -62,7 +135,7 @@ def offline_model_performance(estimator, validation_feature, validation_label, p
             else:
                 continue
             fpr2 = fpr_1
-            print("tpr at 0.005 selected tpr_1:{}, fpr_1:{}, fpr_2:{}, tpr_2:{}".format(tpr_1, fpr_1, fpr_2, tpr_2))
+            print("tpr at 0.005 selected tpr_1:{:7f}, fpr_1:{:7f}, fpr_2:{:7f}, tpr_2:{:7f}".format(tpr_1, fpr_1, fpr_2, tpr_2))
             tpr2 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(float(0.005)-fpr_1))+tpr_1
         elif fpr[i] >= 0.01 and fpr[i] <= fpr3:
             fpr_1 = fpr[i]
@@ -84,8 +157,9 @@ def offline_model_performance(estimator, validation_feature, validation_label, p
             else:
                 continue
             fpr3 = fpr_1
-            print("tpr at 0.01 selected tpr_1:{}, fpr_1:{}, fpr_2:{}, tpr_2:{}".format(tpr_1, fpr_1, fpr_2, tpr_2))
+            print("tpr at 0.01 selected tpr_1:{:7f}, fpr_1:{:7f}, fpr_2:{:7f}, tpr_2:{:7f}".format(tpr_1, fpr_1, fpr_2, tpr_2))
             tpr3 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(float(0.01)-fpr_1))+tpr_1
+        """
     model_performance = 0.4 * tpr1 + 0.3 * tpr2 + 0.3 * tpr3
     print("\n# Offline model performance ROC : {}".format(model_performance))
     print("\n# Perfromance ROC : <<<{}>>>".format(str(model_performance)) + "\n"
