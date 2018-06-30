ad574ab3038fcda3c0314c031d4f1ba100084d0e
Leo <zhihao.cui@student.uts.edu.au>
2018-06-30 12:04:58 +1000

    v_1.0.1

--- ant/booster.py
+++ ant/booster.py
@@ -107,9 +107,9 @@ def pu_method():
 
 start = time.time()
 classifier = {
-	"XGB" : XGBClassifier(max_depth = 4, n_estimators = 480, subsample = 0.7, gamma = 0,
-						 scale_pos_weight =1, min_child_weight = 2,
-						 colsample_bytree = 0.9, learning_rate = 0.08, n_jobs = -1),
+	"XGB" : XGBClassifier(max_depth = 4, n_estimators = 480, subsample = 0.8, gamma = 0.1,
+						 
+						 colsample_bytree = 0.8, learning_rate = 0.08, n_jobs = -1),
 
   	"logistic_regression" : LogisticRegression(penalty = "l2", C = 1, solver = "newton-cg",
   						 class_weight = "balanced", max_iter = 300, n_jobs = -1),
