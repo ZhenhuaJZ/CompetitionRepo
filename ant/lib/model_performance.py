1a388d6a0a597f751ef65f6904f7085fd80c7958
UTSAccount <zhenhua.zhang@student.uts.edu.au>
2018-06-30 14:19:30 +1000

    change tpr[i-1] to i+1

--- ant/lib/model_performance.py
+++ ant/lib/model_performance.py
@@ -19,32 +19,20 @@ def offline_model_performance(estimator, validation_feature, validation_label, p
         if fpr[i] >= 0.001 and fpr[i] <= fpr1:
             fpr_1 = fpr[i]
             tpr_1 = tpr[i]
-            if fpr[i] - fpr[i-1] > fpr[i] - fpr[i+1]:
-                fpr_2 = fpr[i+1]
-                tpr_2 = tpr[i+1]
-            else:
-                fpr_2 = fpr[i-1]
-                tpr_2 = tpr[i-1]
+            fpr_2 = fpr[i+1]
+            tpr_2 = tpr[i+1]
             tpr1 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(0.001-fpr_1))+tpr_1
         elif fpr[i] >= 0.005 and fpr[i] <= fpr2:
             fpr_1 = fpr[i]
             tpr_1 = tpr[i]
-            if fpr[i] - fpr[i-1] > fpr[i] - fpr[i+1]:
-                fpr_2 = fpr[i+1]
-                tpr_2 = tpr[i+1]
-            else:
-                fpr_2 = fpr[i-1]
-                tpr_2 = tpr[i-1]
+            fpr_2 = fpr[i+1]
+            tpr_2 = tpr[i+1]
             tpr2 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(0.005-fpr_1))+tpr_1
         elif fpr[i] >= 0.01 and fpr[i] <= fpr3:
             fpr_1 = fpr[i]
             tpr_1 = tpr[i]
-            if fpr[i] - fpr[i-1] > fpr[i] - fpr[i+1]:
-                fpr_2 = fpr[i+1]
-                tpr_2 = tpr[i+1]
-            else:
-                fpr_2 = fpr[i-1]
-                tpr_2 = tpr[i-1]
+            fpr_2 = fpr[i+1]
+            tpr_2 = tpr[i+1]
             tpr3 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(0.01-fpr_1))+tpr_1
     model_performance = 0.4 * tpr1 + 0.3 * tpr2 + 0.3 * tpr3
     print("\n# Offline model performance ROC : {}".format(model_performance))
