01dad00415e2a995c5a8b5dd90312776d96204d6
Leo <zhihao.cui@student.uts.edu.au>
2018-06-30 21:20:24 +1000

    change params

--- ant/booster.py
+++ ant/booster.py
@@ -71,7 +71,7 @@ def main():
 
     start = time.time()
     classifier = {
-    	"XGB" : XGBClassifier(max_depth = 4, n_estimators = 480, subsample = 0.8, gamma = 0.1,
+    	"XGB" : XGBClassifier(max_depth = 4, n_estimators = 4, subsample = 0.8, gamma = 0.1,
                                 min_child_weight = 1, scale_pos_weight = 1,
     						 colsample_bytree = 0.8, learning_rate = 0.08, n_jobs = -1),
 
