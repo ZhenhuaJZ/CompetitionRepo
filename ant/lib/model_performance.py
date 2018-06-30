95e5cd0008e84ff47dbcdb024f6c6cf3e9fa6c44
UTSAccount <zhenhua.zhang@student.uts.edu.au>
2018-06-30 14:25:02 +1000

    Added print for tpr and fpr visualisation

--- ant/lib/model_performance.py
+++ ant/lib/model_performance.py
@@ -10,6 +10,9 @@ def offline_model_performance(estimator, validation_feature, validation_label, p
     #params_path = log_path + "params/"
     # Obtain array of false positive rate and true positive rate
     fpr, tpr, thresholds = roc_curve(validation_label, estimator.predict_proba(validation_feature)[:,1])
+    print("fpr list", fpr)
+
+    print("tpr list", tpr)
     # Search for tpr at fpr = 0.001,0.005,0.01
     fpr1 = 99
     fpr2 = 99
@@ -21,33 +24,36 @@ def offline_model_performance(estimator, validation_feature, validation_label, p
             tpr_1 = tpr[i]
             fpr_2 = fpr[i+1]
             tpr_2 = tpr[i+1]
+            print("tpr at 0.001 selected tpr_1:{}, fpr_1:{}, fpr_2:{}, tpr_2:{}".format(tpr_1, fpr_1, tpr_2, fpr_2))
             tpr1 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(0.001-fpr_1))+tpr_1
         elif fpr[i] >= 0.005 and fpr[i] <= fpr2:
             fpr_1 = fpr[i]
             tpr_1 = tpr[i]
             fpr_2 = fpr[i+1]
             tpr_2 = tpr[i+1]
+            print("tpr at 0.005 selected tpr_1:{}, fpr_1:{}, fpr_2:{}, tpr_2:{}".format(tpr_1, fpr_1, tpr_2, fpr_2))
             tpr2 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(0.005-fpr_1))+tpr_1
         elif fpr[i] >= 0.01 and fpr[i] <= fpr3:
             fpr_1 = fpr[i]
             tpr_1 = tpr[i]
             fpr_2 = fpr[i+1]
             tpr_2 = tpr[i+1]
+            print("tpr at 0.01 selected tpr_1:{}, fpr_1:{}, fpr_2:{}, tpr_2:{}".format(tpr_1, fpr_1, tpr_2, fpr_2))
             tpr3 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(0.01-fpr_1))+tpr_1
     model_performance = 0.4 * tpr1 + 0.3 * tpr2 + 0.3 * tpr3
     print("\n# Offline model performance ROC : {}".format(model_performance))
     print("\n# Perfromance ROC : <<<{}>>>".format(str(model_performance)) + "\n"
-          +"# fpr1 : {} ----> to tpr1: {}".format(str(fpr1), str(tpr1)) + "\n"
-          +"# fpr2 : {} ----> to tpr2: {}".format(str(fpr2), str(tpr2)) + "\n"
-          +"# fpr3 : {} ----> to tpr3: {}".format(str(fpr3), str(tpr3)) + "\n"
+          +"# fpr1 : {} ----> to tpr1: {}".format(str(0.001), str(tpr1)) + "\n"
+          +"# fpr2 : {} ----> to tpr2: {}".format(str(0.005), str(tpr2)) + "\n"
+          +"# fpr3 : {} ----> to tpr3: {}".format(str(0.01), str(tpr3)) + "\n"
     )
     with open(params_path  + "params.txt", 'a') as f:
         f.write(
         "**"*40 + "\n"*2
         +"Perfromance ROC : <<<{}>>>".format(str(model_performance)) + "\n"
-        +"fpr1 : {} ----> to tpr1: {}".format(str(fpr1), str(tpr1)) + "\n"
-        +"fpr2 : {} ----> to tpr2: {}".format(str(fpr2), str(tpr2)) + "\n"
-        +"fpr3 : {} ----> to tpr3: {}".format(str(fpr3), str(tpr3)) + "\n"
+        +"fpr1 : {} ----> to tpr1: {}".format(str(0.001), str(tpr1)) + "\n"
+        +"fpr2 : {} ----> to tpr2: {}".format(str(0.005), str(tpr2)) + "\n"
+        +"fpr3 : {} ----> to tpr3: {}".format(str(0.01), str(tpr3)) + "\n"
         +"**"*40 + "\n"*2
         )
     return model_performance
