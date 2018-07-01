f7dd998c35decc5793a106026db2e381aaf8d5a8
Leo <zhihao.cui@student.uts.edu.au>
2018-07-02 02:13:57 +1000

    params

--- ant/lib/data_visualization.py
+++ ant/lib/data_visualization.py
@@ -5,7 +5,7 @@ import seaborn as sns # data visualization library
 import matplotlib.pyplot as plt
 import time
 from subprocess import check_output
-from data_processing import test_train_split_by_date
+from data_processing import *
 
 #correlation map
 def correlation_map():
@@ -70,6 +70,57 @@ def hist_visualization(df_0, df_1, prefix, figure_1, figure_2):
 		plt.savefig(save_path + "{}.png".format(col_name))
 		plt.figure()
 
+
+def time_stamp(date_column):
+    for i in range(len(date_column)):
+        if date_column.date[i] <= 20170915 and date_column.date[i+1] >= 20170916:
+            time_stamp_1 =  i+1
+        elif date_column.date[i] <= 20170930 and  date_column.date[i+1] >= 20171001:
+            time_stamp_2 =  i+1
+        elif date_column.date[i] <= 20171015 and  date_column.date[i+1] >= 20171016:
+            time_stamp_3 =  i+1
+        elif date_column.date[i] <= 20171031 and  date_column.date[i+1] >= 20171101:
+            time_stamp_4 =  i+1
+    return time_stamp_1, time_stamp_2, time_stamp_3, time_stamp_4
+
+def save_features(data_path, prefix, mode = "train"):
+	save_path = "../data/feats/" + prefix + "/"
+	if not os.path.exists(save_path):
+		os.makedirs(save_path)
+		#read data and choose column
+		df = pd.read_csv(data_path)
+		date = df.loc[:,["date"]]
+
+	for i in range(297):
+		col_name = "f"+str(i+1)
+		print(col_name)
+		if mode == "train":
+			df_0 = df[(df.label==0)]
+			df_1 = df[(df.label==1)]
+			feature_0 = df_0.loc[:,[col_name]]
+			plt.plot(feature_0.index, feature_0.values, 'b.')
+			feature_1 = df_1.loc[:,[col_name]]
+			plt.plot(feature_1.index, feature_1.values, 'r.')
+			plt.legend(("white", "black"), loc = 'upper right')
+			t1, t2, t3, t4 = time_stamp(date)
+			draw_time_stamp = lambda x,ymin,ymax: plt.vlines(x, ymin, ymax, linestyles = 'dashed')
+			ymin, ymax = plt.ylim()
+			for i in [t1, t2, t3, t4]:
+				draw_time_stamp(i, ymin, ymax)
+
+		elif mode == "test":
+			feature = df.loc[:,[col_name]]
+			plt.plot(feature_0.index, feature_0.values, 'b.')
+			plt.legend(("test_b"), loc = 'upper right')
+
+
+			plt.ylabel('values')
+			plt.xlabel("sample_number")
+			plt.title(col_name)
+			plt.savefig(save_path + "{}.png".format(col_name))
+			plt.figure()
+		return
+
 def main():
 
 	train_path = "../data/train.csv"
@@ -79,13 +130,12 @@ def main():
 	_test_online = pd.read_csv(test_path)
 
 	_train_data, _test_offline =  test_train_split_by_date(_train_data, 20171010, 20171020)
-
-	#_train = _train_data.iloc[:,3:]
-	del _train_data
+	clear_mermory(_train_data)
 	_test_online = _test_online.iloc[:,2:]
 	_test_offline = _test_offline.iloc[:,3:]
 	#hist_visualization(_train, _test_online, "train_test_b", figure_1="train", figure_2="test_b")
-	hist_visualization(_test_offline, _test_online, "on-off-1010-1020", figure_1="offline", figure_2="online")
+	#hist_visualization(_test_offline, _test_online, "on-off-1010-1020", figure_1="offline", figure_2="online")
+	save_features(test_path, "test_b")
 
 if __name__ == '__main__':
 	main()
