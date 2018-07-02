dfa2c633039a5ebd8bae0bec9bd3809b1f4f562a
Leo <zhihao.cui@student.uts.edu.au>
2018-07-03 01:48:55 +1000

    BIG Change no single model anymore

--- ant/lib/model_performance.py
+++ ant/lib/model_performance.py
@@ -12,17 +12,11 @@ now = datetime.datetime.now()
 
 ############################### Offline performance ######################
 def offline_model_performance(ground_truth, predict, **kwargs):
-    #log_path = "log/date_{}/GS_{}:{}/".format(now.day,now.hour,now.minute)
-    #params_path = log_path + "params/"
     # Obtain array of false positive rate and true positive rate
     fpr, tpr, thresholds = roc_curve(ground_truth, predict)
     # NOTE: upper line
     fpr = fpr[2::2] #[1::2] #odd line (lower line)
     tpr = tpr[2::2]
-    #print("fpr list", fpr[:20])
-    #print("tpr list", tpr[:20])
-    #print("fpr list lens{}".format(len(fpr)))
-    #print("tpr list lens{}".format(len(tpr)))
     # Search for tpr at fpr = 0.001,0.005,0.01
     fpr1 = 99
     fpr2 = 99
@@ -97,78 +91,7 @@ def offline_model_performance(ground_truth, predict, **kwargs):
             #print("tpr at 0.01 selected tpr_1:{:8f}, fpr_1:{:8f}, fpr_2:{:8f}, tpr_2:{:8f}".format(tpr_1, fpr_1, fpr_2, tpr_2))
             tpr3 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(float(0.01)-fpr_1))+tpr_1
 
-    """
-    for i in range(1,len(fpr),2):
-        min_dist_val = 0
-        if fpr[i] >= 0.001 and fpr[i] <= fpr1:
-            fpr_1 = fpr[i]
-            tpr_1 = tpr[i]
-            diff_n_1 = abs(fpr[i] - fpr[i-2])
-            diff_p_1 = abs(fpr[i] - fpr[i+2])
-            if (diff_p_1 < diff_n_1) and (diff_p_1 != 0):
-                fpr_2 = fpr[i+2]
-                tpr_2 = tpr[i+2]
-            elif (diff_n_1 < diff_p_1) and (diff_n_1 != 0):
-                fpr_2 = fpr[i-2]
-                tpr_2 = tpr[i-2]
-            elif diff_p_1:
-                fpr_2 = fpr[i+2]
-                tpr_2 = tpr[i+2]
-            elif diff_n_1:
-                fpr_2 = fpr[i-2]
-                tpr_2 = tpr[i-2]
-            else:
-                continue
-            fpr1 = fpr_1
-            print("tpr at 0.001 selected tpr_1:{:7f}, fpr_1:{:7f}, fpr_2:{:7f}, tpr_2:{:7f}".format(tpr_1, fpr_1, fpr_2, tpr_2))
-            tpr1 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(float(0.001)-fpr_1))+tpr_1
-        elif fpr[i] >= 0.005 and fpr[i] <= fpr2:
-            fpr_1 = fpr[i]
-            tpr_1 = tpr[i]
-            diff_n_1 = abs(fpr[i] - fpr[i-2])
-            diff_p_1 = abs(fpr[i] - fpr[i+2])
-            if (diff_p_1 < diff_n_1) and (diff_p_1 != 0):
-                fpr_2 = fpr[i+2]
-                tpr_2 = tpr[i+2]
-            elif (diff_n_1 < diff_p_1) and (diff_n_1 != 0):
-                fpr_2 = fpr[i-2]
-                tpr_2 = tpr[i-2]
-            elif diff_p_1:
-                fpr_2 = fpr[i+2]
-                tpr_2 = tpr[i+2]
-            elif diff_n_1:
-                fpr_2 = fpr[i-2]
-                tpr_2 = tpr[i-2]
-            else:
-                continue
-            fpr2 = fpr_1
-            print("tpr at 0.005 selected tpr_1:{:7f}, fpr_1:{:7f}, fpr_2:{:7f}, tpr_2:{:7f}".format(tpr_1, fpr_1, fpr_2, tpr_2))
-            tpr2 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(float(0.005)-fpr_1))+tpr_1
-        elif fpr[i] >= 0.01 and fpr[i] <= fpr3:
-            fpr_1 = fpr[i]
-            tpr_1 = tpr[i]
-            diff_n_1 = abs(fpr[i] - fpr[i-2])
-            diff_p_1 = abs(fpr[i] - fpr[i+2])
-            if (diff_p_1 < diff_n_1) and (diff_p_1 != 0):
-                fpr_2 = fpr[i+2]
-                tpr_2 = tpr[i+2]
-            elif (diff_n_1 < diff_p_1) and (diff_n_1 != 0):
-                fpr_2 = fpr[i-2]
-                tpr_2 = tpr[i-2]
-            elif diff_p_1:
-                fpr_2 = fpr[i+2]
-                tpr_2 = tpr[i+2]
-            elif diff_n_1:
-                fpr_2 = fpr[i-2]
-                tpr_2 = tpr[i-2]
-            else:
-                continue
-            fpr3 = fpr_1
-            print("tpr at 0.01 selected tpr_1:{:7f}, fpr_1:{:7f}, fpr_2:{:7f}, tpr_2:{:7f}".format(tpr_1, fpr_1, fpr_2, tpr_2))
-            tpr3 = (((tpr_2-tpr_1)/(fpr_2-fpr_1))*(float(0.01)-fpr_1))+tpr_1
-        """
     model_performance = 0.4 * tpr1 + 0.3 * tpr2 + 0.3 * tpr3
-
 
     print("\n# Offline model performance_1 ROC : <<<{:9f}>>>".format(model_performance) + "\n"
           +"# fpr1 : {} ----> to tpr1: {:9f}".format(0.001, tpr1) + "\n"
@@ -188,8 +111,7 @@ def offline_model_performance(ground_truth, predict, **kwargs):
         +"fpr3 : {} ----> to tpr3: {}".format(str(0.01), str(tpr3)) + "\n"
         +"**"*40 + "\n"*2
         )
-    #if isinstance(kwargs['fold'], int):
-        #+"fold : " + str(kwargs['fold']) +"\n"
+
     return model_performance
 
 def get_tpr_from_fpr(fpr_array, tpr_array, target):
@@ -228,28 +150,12 @@ def offline_model_performance_2(ground_truth, predict, **kwargs):
     tpr2 = get_tpr_from_fpr(fpr, tpr, 0.005)
     tpr3 = get_tpr_from_fpr(fpr, tpr, 0.01)
     model_performance = 0.4*tpr1 + 0.3*tpr2 + 0.3*tpr3
-    """
-    print("\n# Offline model performance_2 ROC : <<<{:9f}>>>".format(model_performance) + "\n"
-          +"# fpr1 : {} ----> to tpr1: {:9f}".format(0.001, tpr1) + "\n"
-          +"# fpr2 : {} ----> to tpr2: {:9f}".format(0.005, tpr2) + "\n"
-          +"# fpr3 : {} ----> to tpr3: {:9f}".format(0.01, tpr3) + "\n"
-    )
 
-    with open(kwargs['params_path']  + "params.txt", 'a') as f:
-        f.write(
-        "**"*40 + "\n"*2
-        +"Offline model performance_2 ROC : <<<{}>>>".format(str(model_performance)) + "\n"
-        +"fpr1 : {} ----> to tpr1: {}".format(str(0.001), str(tpr1)) + "\n"
-        +"fpr2 : {} ----> to tpr2: {}".format(str(0.005), str(tpr2)) + "\n"
-        +"fpr3 : {} ----> to tpr3: {}".format(str(0.01), str(tpr3)) + "\n"
-        +"**"*40 + "\n"*2
-        )
-    """
     return model_performance
 
 # #############################Log all the data ################################
 
-def log_parmas(clf, valset, roc_1, roc_2, mode, filename, fillna, pu_thres = "n/a",
+def log_parmas(clf, valset, roc_1, roc_2, filename, fillna, mode = "PU", pu_thres = "n/a",
                 cv_roc_1_mean = "n/a", cv_roc_2_mean = "n/a", under_samp = False, Feed_val_back = ""):
     #formate log
     valset = str(valset[0]) + "-" + str(valset[1])
