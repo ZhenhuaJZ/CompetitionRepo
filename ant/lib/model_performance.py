dc97ac05c6d8aea9bdc92c59505d606e30661b9f
Leo <zhihao.cui@student.uts.edu.au>
2018-07-02 17:41:57 +1000

    cv-5

--- ant/lib/model_performance.py
+++ ant/lib/model_performance.py
@@ -176,6 +176,8 @@ def offline_model_performance(ground_truth, predict, **kwargs):
     with open(kwargs['params_path']  + "params.txt", 'a') as f:
         f.write(
         "**"*40 + "\n"*2
+        if trpe(kwargs['fold']) == int:
+            +"fold : " + str(kwargs['fold']) +"\n"
         +"Offline model performance_1 ROC(JL) : <<<{}>>>".format(str(model_performance)) + "\n"
         +"fpr1 : {} ----> to tpr1: {}".format(str(0.001), str(tpr1)) + "\n"
         +"fpr2 : {} ----> to tpr2: {}".format(str(0.005), str(tpr2)) + "\n"
@@ -229,6 +231,8 @@ def offline_model_performance_2(ground_truth, predict, **kwargs):
     with open(kwargs['params_path']  + "params.txt", 'a') as f:
         f.write(
         "**"*40 + "\n"*2
+        if trpe(kwargs['fold']) == int:
+            +"fold : " + str(kwargs['fold']) +"\n"
         +"Offline model performance_2 ROC : <<<{}>>>".format(str(model_performance)) + "\n"
         +"fpr1 : {} ----> to tpr1: {}".format(str(0.001), str(tpr1)) + "\n"
         +"fpr2 : {} ----> to tpr2: {}".format(str(0.005), str(tpr2)) + "\n"
