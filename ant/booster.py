6bca05b142032dc7691fa23e902511dfe8d3b5cb
Leo <zhihao.cui@student.uts.edu.au>
2018-07-03 00:24:37 +1000

    feed black back

--- ant/booster.py
+++ ant/booster.py
@@ -93,7 +93,7 @@ def main():
 					pu_thres = p, method = method, cv = cv, fold_time_split = fold_time_split, under_samp = under_samp)
 	else:
 		classifier = {
-		"XGB" : XGBClassifier(max_depth = 4, n_estimators = 4, subsample = 0.8, gamma = 0,
+		"XGB" : XGBClassifier(max_depth = 4, n_estimators = 480, subsample = 0.8, gamma = 0,
 		min_child_weight = 1, scale_pos_weight = 1,
 		colsample_bytree = 0.8, learning_rate = 0.07, n_jobs = -1),
 
