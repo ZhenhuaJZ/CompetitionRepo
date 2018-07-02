cd7f952ecc575b4f6e66d97e6311b550d2ef5672
Leo <zhihao.cui@student.uts.edu.au>
2018-07-02 19:10:17 +1000

    debug

--- ant/booster.py
+++ ant/booster.py
@@ -59,7 +59,7 @@ def main():
 			core(fillna, log_path, offline_validation, clf, train_path, test_path, test_a_path, method = method, cv = cv)
 	else:
 		classifier = {
-		"XGB" : XGBClassifier(max_depth = 4, n_estimators = 480, subsample = 0.8, gamma = 0,
+		"XGB" : XGBClassifier(max_depth = 4, n_estimators = 4, subsample = 0.8, gamma = 0,
 		min_child_weight = 1, scale_pos_weight = 1,
 		colsample_bytree = 0.8, learning_rate = 0.07, n_jobs = -1),
 
