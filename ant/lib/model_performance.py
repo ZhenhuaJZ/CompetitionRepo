bc7292dd69ca8019a3a693fa3ac553619c5efdf7
Leo <zhihao.cui@student.uts.edu.au>
2018-07-02 17:46:01 +1000

    cv-5

--- ant/lib/model_performance.py
+++ ant/lib/model_performance.py
@@ -176,7 +176,7 @@ def offline_model_performance(ground_truth, predict, **kwargs):
     with open(kwargs['params_path']  + "params.txt", 'a') as f:
         f.write(
         "**"*40 + "\n"*2
-        if trpe(kwargs['fold']) == int:
+        if type(kwargs['fold']) == int:
             +"fold : " + str(kwargs['fold']) +"\n"
         +"Offline model performance_1 ROC(JL) : <<<{}>>>".format(str(model_performance)) + "\n"
         +"fpr1 : {} ----> to tpr1: {}".format(str(0.001), str(tpr1)) + "\n"
@@ -231,7 +231,7 @@ def offline_model_performance_2(ground_truth, predict, **kwargs):
     with open(kwargs['params_path']  + "params.txt", 'a') as f:
         f.write(
         "**"*40 + "\n"*2
-        if trpe(kwargs['fold']) == int:
+        if type(kwargs['fold']) == int:
             +"fold : " + str(kwargs['fold']) +"\n"
         +"Offline model performance_2 ROC : <<<{}>>>".format(str(model_performance)) + "\n"
         +"fpr1 : {} ----> to tpr1: {}".format(str(0.001), str(tpr1)) + "\n"
