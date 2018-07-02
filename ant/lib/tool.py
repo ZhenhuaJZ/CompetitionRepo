2e20a99c0ea51bf7b616c60aa014628fdb00d309
Leo <zhihao.cui@student.uts.edu.au>
2018-07-02 22:20:12 +1000

    tool

---
+++ ant/lib/tool.py
@@ -0,0 +1,20 @@
+import os, sys
+
+def double_check(*args, **kwargs):
+    print("\n# CheckList : " )
+    print(args[0].keys()[0])
+    for i, kwarg in enumerate(kwargs):
+        print("\n** {} is : {} ".format(kwarg, kwargs[kwarg]))
+
+    check = input("\n# Y/N ? :").lower()
+    if check in ["y", "yes"] :
+        print("\n# Pass")
+        pass
+    elif check in ("n", "no", "false"):
+        print("\n# Exit")
+        return sys.exit()
+
+
+p = { "a" : 1, "b" : 2, "c" : 3}
+
+double_check(p)
