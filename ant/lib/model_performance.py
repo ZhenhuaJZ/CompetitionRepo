b785ca1c9a30ef0b6ffc66c7dc3ba17eae40a2e4
Leo <zhihao.cui@student.uts.edu.au>
2018-07-03 22:11:56 +1000

    change code format

--- ant/lib/model_performance.py
+++ ant/lib/model_performance.py
@@ -155,7 +155,7 @@ def offline_model_performance_2(ground_truth, predict, **kwargs):
 
 # #############################Log all the data ################################
 
-def log_parmas(clf, valset, roc_1, roc_2, cv_roc_1_mean="n/a", cv_roc_1_mean ="n/a",  **kwargs):
+def log_parmas(clf, valset, roc_1, roc_2, cv_roc_1_mean="n/a", cv_roc_2_mean ="n/a",  **kwargs):
     #formate log
     valset = str(valset[0]) + "-" + str(valset[1])
     roc_1 = round(roc_1, 6)
