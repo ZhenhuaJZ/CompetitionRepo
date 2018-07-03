3297e9947abec6378b4fa1faf8dfb6b450df1f27
Leo <zhihao.cui@student.uts.edu.au>
2018-07-03 16:22:07 +1000

    debug

--- ant/core_model.py
+++ ant/core_model.py
@@ -150,14 +150,15 @@ def core(fillna, log_path, offline_validation, clf, train_path, test_path, test_
 
 	print(test_b_seg_1)
 	print(test_b_seg_1["id"])
-	test_b_seg_1["id"].assign(score = score_seg_1[:,1])
+	test_b_seg_1 = pd.DataFrame(test_b_seg_1["id"])
+	test_b_seg_1.assign(score = score_seg_1[:,1])
 
 	increment_train = merged_file(test_b_seg_1_black, _final_train, "date")
 	increment_train_feature, increment_train_label = split_train_label(increment_train)
 	clf = clf.fit(increment_train_feature, increment_train_label)
 	score_seg_2 = clf.predict_proba(test_b_seg_2.iloc[:,2])
-
-	test_b_seg_2.loc["id"].assign(score = test_b_seg_2[:,1])
+	test_b_seg_2 = pd.DataFrame(test_b_seg_2["id"])
+	test_b_seg_2.assign(score = test_b_seg_2[:,1])
 
 	# TODO:  merge score_seg_a and score_seg_b
 	score = score_seg_2[:,1] + score_seg_1[:,1]
