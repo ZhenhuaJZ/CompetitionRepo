4cc0ffa786d652c8dd5fd447df6531c1ef7cacf6
Leo <zhihao.cui@student.uts.edu.au>
2018-07-04 07:10:34 +1000

    debug

--- ant/debug.py
+++ ant/debug.py
@@ -3,9 +3,8 @@ from lib.data_processing import *
 from lib.model_performance import *
 import sys
 
-#data_1 = pd.DataFrame([[1,2,3,4],[2,5,6,7]], columns=['date','f1','f2','f3'])
-#data_2 = pd.DataFrame([[3,2,3,6],[4,5,6,7]],columns=['date','f1','f2','f3'])
 
+# DEBUG: for file merge
 data_1 = "_train_data_test.csv"
 data_2 = "pu_black_data_test.csv"
 
@@ -14,3 +13,5 @@ df2 = pd.read_csv(data_2)
 
 merged_file = file_merge(df1, df2, "date")
 print(merged_file)
+
+## DEBUG: for call fit and add call DataFrame
