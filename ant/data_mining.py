500ffdabfef714b15df677eb504e4350988e231a
Leo <zhihao.cui@student.uts.edu.au>
2018-07-05 03:00:33 +1000

    parmas

--- ant/data_mining.py
+++ ant/data_mining.py
@@ -15,8 +15,8 @@ test_a_path = "data/test_a.csv"
 validation_path = "data/_test_offline.csv"
 
 
-pu_thresh_a = 0.7 #PU threshold for testa
-pu_thresh_b = 0.85 #PU threshold for testb
+pu_thresh_a = 0.8 #PU threshold for testa
+pu_thresh_b = 0.80 #PU threshold for testb
 partial_rate = 0.4
 ################################################################################
 ## DEBUG:
@@ -26,8 +26,8 @@ debug = False
 def init_train(eval = True, save_score = False):
 
     start = time.time()
-    clf = XGBClassifier(max_depth = 4, n_estimators = 4, subsample = 0.8, gamma = 0.1,
-                    min_child_weight = 1, scale_pos_weight = 10,
+    clf = XGBClassifier(max_depth = 4, n_estimators = 480, subsample = 0.7, gamma = 0.1,
+                    min_child_weight = 1, scale_pos_weight = 1,
                     colsample_bytree = 0.8, learning_rate = 0.07, n_jobs = -1)
     #Train
     print("\n# Start Traing")
