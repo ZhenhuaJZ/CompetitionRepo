0aa09e00e2d5f7776b69b7b7a599d4471424f9d9
Leo <zhihao.cui@student.uts.edu.au>
2018-07-04 22:17:44 +1000

    fast_ver

--- ant/booster.py
+++ ant/booster.py
@@ -26,7 +26,7 @@ def main():
 	tunning = False
 	tuning_range = [0.5,0.6]
 
-	pu_thres = 0.5
+	pu_thres = 0.8
 	offline_validation = [20171025, 20171105] #20171025, 20171105
 	#CV
 	cv = False
--- ant/core_model.py
+++ ant/core_model.py
@@ -193,7 +193,7 @@ def core(fillna, log_path, offline_validation, clf, train_path, test_path, test_
         clear_mermory(_test_online)
 
         #PU for test_b
-        test_b_seg_1_black = positive_unlabel_learning(clf, test_b_seg_1, 0.95) #pu threshold
+        test_b_seg_1_black = positive_unlabel_learning(clf, test_b_seg_1, 0.97) #pu threshold
         clear_mermory(test_b_seg_1)
         increment_train = file_merge(test_b_seg_1_black, _final_train, "date")
         clear_mermory(test_b_seg_1_black, _final_train)
