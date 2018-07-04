0ea4ef42986e7530782f20dec0e184f46e340cec
Leo <zhihao.cui@student.uts.edu.au>
2018-07-04 11:20:52 +1000

    debug

--- ant/core_model.py
+++ ant/core_model.py
@@ -137,7 +137,6 @@ def core(fillna, log_path, offline_validation, clf, train_path, test_path, test_
 	# NOTE: PU learning
 	_test_a = df_read_and_fillna(test_a_path, fillna)
 	pu_black_data = positive_unlabel_learning(clf, _test_a, pu_thres)
-	print(pu_black_data)
 	clear_mermory(_test_a)
 
 	pu_train_data = file_merge(_train_data, pu_black_data, "date")
@@ -152,7 +151,7 @@ def core(fillna, log_path, offline_validation, clf, train_path, test_path, test_
 		cv_clf = clf
 		print("\n# 5-Fold CV (Evaluation Classifier)")
 		roc_1_mean, roc_2_mean = cv_fold(cv_clf, pu_train_data, fold_time_split, params_path)
-	print(_test_offline_feature)
+		
 	offline_probs = clf.predict_proba(_test_offline_feature)
 	#evl pu model
 	offline_score_1 = offline_model_performance(_test_offline_labels, offline_probs[:,1], params_path = params_path)
