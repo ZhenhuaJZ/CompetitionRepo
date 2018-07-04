707a54dc79603fee8cb2dc080876740d0654217c
Leo <zhihao.cui@student.uts.edu.au>
2018-07-04 12:39:51 +1000

    debug

--- ant/debug.py
+++ ant/debug.py
@@ -10,10 +10,10 @@ _train_data = custom_imputation(_train_data)
 _train_data = _train_data.replace({"label" : -1}, value = 1)
 _train_data.info(memory_usage='deep')
 
-_train_data = _train_data.iloc[:,1:].astype('int')
-
-
+_train_data.iloc[:,1:].astype('int')
+_train_data.loc["id"].astype('category')
 _train_data.info(memory_usage='deep')
+print(_train_data)
 
 for dtype in ['float','int','object']:
     selected_dtype = _train_data.select_dtypes(include=[dtype])
