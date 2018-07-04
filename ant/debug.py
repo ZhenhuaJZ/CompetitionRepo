9a23031fab54a2db28e49a1e491c2fc12364ae47
Leo <zhihao.cui@student.uts.edu.au>
2018-07-04 12:11:30 +1000

    debug

--- ant/debug.py
+++ ant/debug.py
@@ -3,9 +3,9 @@ from lib.data_processing import *
 from lib.model_performance import *
 import sys
 
-
-    train_path = "data/train.csv"
-	_train_data = pd.read_csv(train_path)
-	_train_data = custom_imputation(_train_data)
-	_train_data = _train_data.replace({"label" : -1}, value = 1)
-    _train_data.info(memory_usage='deep')
+## DEBUG: 
+train_path = "data/train.csv"
+_train_data = pd.read_csv(train_path)
+_train_data = custom_imputation(_train_data)
+_train_data = _train_data.replace({"label" : -1}, value = 1)
+_train_data.info(memory_usage='deep')
