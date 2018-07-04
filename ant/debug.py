da2abe8d5781cfdbd4daa1146c96425edf4cc3ce
Leo <zhihao.cui@student.uts.edu.au>
2018-07-04 12:18:14 +1000

    debug

--- ant/debug.py
+++ ant/debug.py
@@ -3,9 +3,15 @@ from lib.data_processing import *
 from lib.model_performance import *
 import sys
 
-## DEBUG: 
+## DEBUG:
 train_path = "data/train.csv"
 _train_data = pd.read_csv(train_path)
 _train_data = custom_imputation(_train_data)
 _train_data = _train_data.replace({"label" : -1}, value = 1)
 _train_data.info(memory_usage='deep')
+
+for dtype in ['float','int','object']:
+    selected_dtype = _train_data.select_dtypes(include=[dtype])
+    mean_usage_b = selected_dtype.memory_usage(deep=True).mean()
+    mean_usage_mb = mean_usage_b / 1024 ** 2
+    print("Average memory usage for {} columns: {:03.2f} MB".format(dtype,mean_usage_mb))
