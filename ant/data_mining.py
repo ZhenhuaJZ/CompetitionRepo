c5f45bed9ee84e0a1126eb0bd7f0bb572d331635
Leo <zhihao.cui@student.uts.edu.au>
2018-07-06 08:56:17 +1000

    debug

--- ant/data_mining.py
+++ ant/data_mining.py
@@ -23,7 +23,7 @@ pu_thresh_a = 0.50 #PU threshold for testa
 pu_test_b = False
 pu_thresh_b = 0.85 #PU threshold for testb
 seg_date = 20180215
-params = { "n_estimators" : [485,490], "learning_rate" : [0.07, 0.08]} # params = None "subsample" : [0.7, 0.8]
+params = None #{ "n_estimators" : [485,490], "learning_rate" : [0.07, 0.08]} # params = None "subsample" : [0.7, 0.8]
 
 
 def positive_unlabel_learning(clf, data_path, train, thresh, eval = True, save_score = True, prefix = "pu"):
