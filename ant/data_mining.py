b1684aa9d7bcfd93f6f0ad81ec788b5068a575f9
Leo <zhihao.cui@student.uts.edu.au>
2018-07-05 15:01:16 +1000

    debug

--- ant/data_mining.py
+++ ant/data_mining.py
@@ -15,8 +15,8 @@ test_a_path = "data/test_a.csv"
 validation_path = "data/_test_offline.csv"
 
 
-pu_thresh_a = 0.80 #PU threshold for testa
-pu_thresh_b = 0.85 #PU threshold for testb
+pu_thresh_a = 0.85 #PU threshold for testa
+pu_thresh_b = 0.80 #PU threshold for testb
 seg_date = 20180215
 ################################################################################
 ## DEBUG:
