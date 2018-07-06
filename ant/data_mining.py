4d4b64b98e8eeacdb745530112112ef75a374276
Leo <zhihao.cui@student.uts.edu.au>
2018-07-06 15:52:40 +1000

    parmas

--- ant/data_mining.py
+++ ant/data_mining.py
@@ -11,9 +11,9 @@ now = datetime.datetime.now()
 score_path = "log/last_3_days/{}d_{}h_{}m/".format(now.day, now.hour, now.minute)
 params_path = "log/last_3_days/log_{}h.csv".format(now.hour)
 
-train_path = "data/train_int32.csv"  #train.csv
+train_path = "data/train_normal_un.csv"  #train.csv
 #unlabel_path = "data/unlabel.csv"
-validation_path = "data/validation_int32.csv" #validation.csv
+validation_path = "data/validation_normal_un.csv" #validation.csv
 test_b_path = "data/test_b.csv"
 test_a_path = "data/test_a.csv"
 
