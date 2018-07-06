9b4b91662e709d8a96dd9b57de4a8ac6bb8de2ad
Leo <zhihao.cui@student.uts.edu.au>
2018-07-06 23:12:42 +1000

    parmas

--- ant/debug.py
+++ ant/debug.py
@@ -23,10 +23,4 @@ for dtype in ['float','int','object']:
     mean_usage_mb = mean_usage_b / 1024 ** 2
     print("Average memory usage for {} columns: {:03.2f} MB".format(dtype,mean_usage_mb))
 """
-di = {"a" : 1, "b" : 2, "c" :3}
-
-print(di)
-
-t = [(k,di[k]) for k in sorted(di.keys())]
-
-print(t[0][1])
+def test():
