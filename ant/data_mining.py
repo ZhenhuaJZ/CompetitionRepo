beec9b2645598f45258ac0ff7f0914b6e4ff015b
Leo <zhihao.cui@student.uts.edu.au>
2018-07-06 14:24:45 +1000

    parmas

--- ant/data_mining.py
+++ ant/data_mining.py
@@ -4,6 +4,7 @@ from lib.data_processing import *
 from lib.model_performance import *
 from core_model import pu_labeling, partical_fit, cv_fold, grid_search_roc
 from sklearn.externals import joblib
+from sklearn.base import clone
 import time, sys, datetime
 now = datetime.datetime.now()
 
@@ -23,7 +24,7 @@ pu_thresh_a = 0.50 #PU threshold for testa
 pu_test_b = False
 pu_thresh_b = 0.85 #PU threshold for testb
 seg_date = 20180215
-params =  { "max_depth" : [3, 4], "min_child_weight" : [1, 2], } # params = None "subsample" : [0.7, 0.8]
+params =  { "max_depth" : [3, 4], } # params = None "subsample" : [0.7, 0.8] "min_child_weight" : [1, 2]
 
 def positive_unlabel_learning(clf, data_path, train, thresh, eval = True, save_score = True, prefix = "pu"):
 
@@ -170,7 +171,7 @@ def part_fit(clf, train, seg_date, pu_thresh_b, eval = True, save_score = True):
 
 def pu_a():
 
-    _clf = XGBClassifier(max_depth = 4, n_estimators = 2, subsample = 0.8, gamma = 0,
+    _clf = XGBClassifier(max_depth = 4, n_estimators = 400, subsample = 0.8, gamma = 0,
                     min_child_weight = 1, scale_pos_weight = 1,
                     colsample_bytree = 0.8, learning_rate = 0.07, n_jobs = -1)
 
@@ -207,8 +208,8 @@ def pu_b(train, pu_test_b, eval):
 def main():
     os.makedirs(score_path)
     print("\n# Make dirs in {}".format(score_path))
-    print("Train_path : {}".format(train_path))
-    print("Validation_path : {}".format(validation_path))
+    print("\n# Train_path : {}".format(train_path))
+    print("\n# Validation_path : {}".format(validation_path))
 
     train = pu_a()
     pu_b(train, pu_test_b, eval = False)
