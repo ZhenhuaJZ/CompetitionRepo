74abd63613edc8e9d5a7b12525f1718450b5cdbe
Leo <zhihao.cui@student.uts.edu.au>
2018-07-06 13:59:00 +1000

    debug

--- ant/lib/tool.py
+++ ant/lib/tool.py
@@ -2,7 +2,7 @@ import os, sys
 
 def double_check(dict, **kwargs):
     print("\n# CheckList : " )
-    
+
     for i in dict:
         print("\n** {} is : {} ".format(i, dict[i]))
     for i, kwarg in enumerate(kwargs):
@@ -15,3 +15,19 @@ def double_check(dict, **kwargs):
     elif check in ("n", "no", "false"):
         print("\n# Exit")
         return sys.exit()
+
+
+class Logger(object):
+    def __init__(self, filename = "Logger.log"):
+        self.terminal = sys.stdout
+        self.log = open(filename, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+    def flush(self):
+        pass
+
+sys.stdout = Logger("leo.txt")
+print("hello word")
+print("helleo")
