1c40b9a765bf5917c0804911bfe715a777d0fb72
Leo <zhihao.cui@student.uts.edu.au>
2018-07-07 21:56:58 +1000

    debug

--- ant/debug.py
+++ ant/debug.py
@@ -23,7 +23,9 @@ for dtype in ['float','int','object']:
     mean_usage_mb = mean_usage_b / 1024 ** 2
     print("Average memory usage for {} columns: {:03.2f} MB".format(dtype,mean_usage_mb))
 """
-a = np.array([[1,2,3,4], [5,6,7,8]])
-print(a)
+a = [[1,2,3,4],[1,2,3,4]]
+
+b = np.array(a)
+print(b)
 final_preds = np.average(a, axis =0, weights=[3./4, 1./4])
 print(final_preds)
