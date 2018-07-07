9449cd89e93678ca4e3afb478e31a4479855f939
Leo <zhihao.cui@student.uts.edu.au>
2018-07-07 22:18:36 +1000

    stacking 1

--- ant/stack.py
+++ ant/stack.py
@@ -214,12 +214,12 @@ def two_layer_stacking(train_data, test):
     # ####################First Layer Start#####################
     clf_names = ["XGB", "RF", "LR", "ET", "GBDT"]
     classifier = [
-            XGBClassifier(n_estimators=300, max_depth=3, learning_rate = 0.06, #380
+            XGBClassifier(n_estimators=1, max_depth=3, learning_rate = 0.06, #380
                     gamma = 0.1, n_jobs = -1, subsample = 0.8, colsample_bytree = 0.8),
-            RandomForestClassifier(n_estimators = 260, min_samples_split = 110, max_depth = 20, criterion='entropy', n_jobs = -1), #160
-            LogisticRegression(class_weight = "balanced", C = 1),
-            ExtraTreesClassifier(n_estimators = 200, n_jobs = -1, min_samples_split = 50),
-            GradientBoostingClassifier(n_estimators = 200, learning_rate = 0.08),
+            RandomForestClassifier(n_estimators = 1, min_samples_split = 110, max_depth = 20, criterion='entropy', n_jobs = -1), #160
+            #LogisticRegression(class_weight = "balanced", C = 1),
+            #ExtraTreesClassifier(n_estimators = 200, n_jobs = -1, min_samples_split = 50),
+            #GradientBoostingClassifier(n_estimators = 200, learning_rate = 0.08),
     ]
 
     feature, test = stack_layer(clf_names, classifier, feature, label, test, layer_name = "layer1")
@@ -227,11 +227,11 @@ def two_layer_stacking(train_data, test):
     layer2_clf_names = ["XGB", "RF"]
 
     layer2_classifier = [
-        XGBClassifier(n_estimators=480, max_depth=4, learning_rate = 0.07,
+        XGBClassifier(n_estimators=1, max_depth=4, learning_rate = 0.07,
                           gamma = 0, n_jobs = -1,
                           subsample = 0.8, colsample_bytree = 0.8),
 
-        RandomForestClassifier(n_estimators = 260, min_samples_split = 110, max_depth = 20, criterion='entropy', n_jobs = -1),
+        RandomForestClassifier(n_estimators = 1, min_samples_split = 110, max_depth = 20, criterion='entropy', n_jobs = -1),
     ]
 
     feature, test = stack_layer(layer2_clf_names, layer2_classifier, feature, label, test, layer_name = "layer2")
