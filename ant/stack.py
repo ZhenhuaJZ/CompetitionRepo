867d74864ea206f0d5828a910b68cf283f53c05b
Leo <zhihao.cui@student.uts.edu.au>
2018-07-08 03:06:29 +1000

    stacking 1

--- ant/stack.py
+++ ant/stack.py
@@ -234,7 +234,7 @@ def two_layer_stacking(train_data, test):
             #LogisticRegression(class_weight = "balanced", C = 1),
             MLPClassifier(hidden_layer_sizes=(256,128,128), activation = "logistic", batch_size = 20000),
             XGBClassifier(n_estimators=40, max_depth=3, learning_rate = 0.07, #380
-                    #gamma = 0, n_jobs = -1, subsample = 0.8, colsample_bytree = 0.8),
+                    gamma = 0, n_jobs = -1, subsample = 0.8, colsample_bytree = 0.8),
             #RandomForestClassifier(n_estimators = 400, min_samples_split = 110, max_depth = 25, criterion='entropy', n_jobs = -1), #160
             #ExtraTreesClassifier(n_estimators = 320, n_jobs = -1, min_samples_split = 70),
             #GradientBoostingClassifier(n_estimators = 330, learning_rate = 0.08),
