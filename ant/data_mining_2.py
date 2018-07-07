3e566e6f9fe95609e31a1e62bbbb8fcdb7356cbf
Leo <zhihao.cui@student.uts.edu.au>
2018-07-07 18:03:43 +1000

    debug

--- ant/data_mining_2.py
+++ ant/data_mining_2.py
@@ -22,7 +22,7 @@ corr_data = "data/corr_data.npy"
 
 model_path =  None
 stacking = True
-over_samp = False
+over_samp = True
 pu_test_b = True
 
 over_samp_ratio = 0.02 # 0.06 add 808 to train
@@ -30,16 +30,8 @@ thresh_a = 0.65 #PU threshold for testa
 thresh_b = 0.8 #PU threshold for testb
 seg_date = 20180215
 params = None
-#{"gamma" : [0, 0.1]}
+
 feature_drops = np.load(corr_data).tolist()
-"""
-feature_drops = ["f21", "f22", "f23", "f25","f26", "f27","f33", "f34",
-                        "f49", "f50", "f51",
-                        "f61", "f63", "f65", "f68", "f69","f70","f71",
-                        "f106", "f105",
-                    "f104", "f103", "f154", "f153", "f152",
-                    "f286", "f287"]
-"""
 xgb_a = XGBClassifier(max_depth = 4, n_estimators = 4, subsample = 0.8, gamma = 0,
                 min_child_weight = 1, scale_pos_weight = 1,
                 colsample_bytree = 0.8, learning_rate = 0.07, n_jobs = -1)
@@ -178,10 +170,10 @@ def main():
             _train = _train.drop(feature_drops, axis = 1)
             test_b = test_b.drop(feature_drops, axis = 1)
 
-        _train.iloc[:,1:] = _train.iloc[:,1:].astype('int32')
-        _train.iloc[:,0] = _train.iloc[:,0].astype('category')
-        test_b.iloc[:,1:] = test_b.iloc[:,1:].astype('int32')
-        test_b.iloc[:,0] = test_b.iloc[:,0].astype('category')
+        #_train.iloc[:,1:] = _train.iloc[:,1:].astype('int32')
+        #_train.iloc[:,0] = _train.iloc[:,0].astype('category')
+        #test_b.iloc[:,1:] = test_b.iloc[:,1:].astype('int32')
+        #test_b.iloc[:,0] = test_b.iloc[:,0].astype('category')
 
         probs = two_layer_stacking(_train, test_b)
 
