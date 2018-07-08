279069282c5edf685faf9303036c1dfbf3a8be59
Leo <zhihao.cui@student.uts.edu.au>
2018-07-08 21:21:16 +1000

    read layer 2

--- ant/stack.py
+++ ant/stack.py
@@ -276,8 +276,8 @@ def two_layer_stacking(train_data, test):
 ####################################################################################################
 
 def read_saved_layer(train_data, test, label, score_id):
-    train_data = train_data.drop(train_data.index[:4500])
-    label = label.drop(train_data.index[:4500])
+    train_data = train_data.drop(train_data.index[:4000])
+    label = label.drop(train_data.index[:4000])
     print(train_data)
     train_data = train_data.values
     test = test.values
@@ -291,7 +291,6 @@ def read_saved_layer(train_data, test, label, score_id):
     #magic_feature = _label[:,mg_f] #feature 6, 209, 5, 46, 20, 45, 247, 233
     #magic_test = _score_id[:,mg_f-1] #
     #Append magic feature
-    """
     for mg_f in [5, 208, 4, 45, 19]:
 
         print("\n# Magic feature is {} : ".format("f"+ str(mg_f + 1)))
@@ -309,7 +308,7 @@ def read_saved_layer(train_data, test, label, score_id):
 
         feature = np.append(magic_feature, feature, axis = 1)
         test = np.append(magic_test, test, axis = 1)
-    """
+
     print("\n# Read saved layer data  !! ")
     layer2_clf_names = ["XGB", "RF"]
 
