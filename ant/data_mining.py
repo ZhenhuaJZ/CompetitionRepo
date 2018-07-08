001b324801c2b229383ea9e1fe1e1417c26a7b33
Leo <zhihao.cui@student.uts.edu.au>
2018-07-08 11:54:50 +1000

    read layer 2

--- ant/data_mining.py
+++ ant/data_mining.py
@@ -238,6 +238,9 @@ def main():
 
     if stacking:
 
+        train = pd.read_csv(train_path,low_memory = False)
+        test_b = pd.read_csv(test_b_path)
+        
         probs = two_layer_stacking(train, test_b)
 
         score = pd.DataFrame(test_b["id"]).assign(score = probs)
