427dadcca1ccccf4275400684241b2ee2b16cb20
Leo <zhihao.cui@student.uts.edu.au>
2018-07-08 17:09:31 +1000

    read layer 2

--- ant/stack.py
+++ ant/stack.py
@@ -284,12 +284,15 @@ def read_saved_layer(train_data, test, label):
     label = _label[:,1].astype(int)
 
     feature_new, test_feature_new = select_feature_from_xgb(feature, label, test)
-    print("feature_new", feature_new)
-    feature_new = pd.Series(feature_new[0])
-    test_feature_new = pd.Series(test_feature_new[0])
+    print("feature_new\n", feature_new[0])
 
-    normalized_feature_new = (feature_new-feature_new.min())/(feature_new.max()-feature_new.min())
-    normalized_feature_new = (test_feature_new-test_feature_new.min())/(test_feature_new.max()-test_feature_new.min())
+    feature_new = pd.Series(feature_new[0])
+    print(feature_new)
+    sys.exit()
+    #test_feature_new = pd.Series(test_feature_new[0])
+
+    #normalized_feature_new = (feature_new-feature_new.min())/(feature_new.max()-feature_new.min())
+    #normalized_feature_new = (test_feature_new-test_feature_new.min())/(test_feature_new.max()-test_feature_new.min())
 
     print("\n# Read saved layer data  !! ")
     layer2_clf_names = ["XGB", "RF"]
