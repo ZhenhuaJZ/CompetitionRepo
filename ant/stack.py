d04a3301e23b70bc573ba9879a635bedf8c986d7
Leo <zhihao.cui@student.uts.edu.au>
2018-07-08 18:09:16 +1000

    read layer 2

--- ant/stack.py
+++ ant/stack.py
@@ -294,8 +294,8 @@ def read_saved_layer(train_data, test, label, score_id):
     magic_test = (magic_test-magic_test.min())/(magic_test.max()-magic_test.min())
     magic_test = np.array(magic_test)
 
-    feature = np.hstack(magic_feature, feature)
-    test = np.hstack(magic_test, test)
+    feature = np.hstack((magic_feature, feature))
+    test = np.hstack((magic_test, test))
     print(feature)
     print(feature.shape)
     sys.exit()
