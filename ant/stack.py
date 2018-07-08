5fee43353b5f42aca4653ca98aee2dcf6b2c2a60
Leo <zhihao.cui@student.uts.edu.au>
2018-07-08 19:36:46 +1000

    read layer 2

--- ant/stack.py
+++ ant/stack.py
@@ -277,6 +277,7 @@ def two_layer_stacking(train_data, test):
 
 def read_saved_layer(train_data, test, label, score_id):
     train_data = train_data.drop(train_data.index[:4000])
+    label = label.drop(train_data.index[:4000])
     print(train_data)
     train_data = train_data.values
     test = test.values
